3afbb251aefaf0059fa20436d9b644ef6dac688a
LeeYoonSang <kty189@naver.com>
2019-11-17 20:48:04 +0900

    ch02 완료

--- ch02/src/App.js
+++ ch02/src/App.js
@@ -1,24 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Fragment } from 'react';
 import './App.css';
 
 function App() {
+  const name = "리액트다";
+  const style = {
+    backgroundColor: 'black',
+    color: 'aqua',
+    fontSize: '48px',
+    padding: 16
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="react">
+      <h1>{name}</h1>
     </div>
   );
 }
